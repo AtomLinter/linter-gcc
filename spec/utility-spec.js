a54de248f0e4a5a736aeabfa4e15d8cbb529782f
hebaishi <hebaishi.husam@gmail.com>
2016-02-04 20:43:44 +0000

    Add lint on-the-fly

---
+++ spec/utility-spec.js
@@ -0,0 +1,51 @@
+"use babel";
+
+describe('Utility functions', () => {
+  const lint = require('../lib/main.js')
+  var settings = require("../lib/config").settings
+
+  beforeEach(() => {
+    waitsForPromise(() => {
+      atom.config.set('linter-gcc.execPath', '/usr/bin/g++')
+      atom.config.set('linter-gcc.gccDefaultCFlags', '-Wall')
+      atom.config.set('linter-gcc.gccDefaultCppFlags', '-Wall -std=c++11')
+      atom.config.set('linter-gcc.gccErrorLimit', 15)
+      atom.config.set('linter-gcc.gccIncludePaths', ' ')
+      atom.config.set('linter-gcc.gccSuppressWarnings', true)
+      atom.config.set('linter-gcc.gccLintOnTheFly', false)
+      atom.packages.activatePackage("language-c")
+      atom.packages.activatePackage("language-javascript")
+      return atom.packages.activatePackage('linter-gcc')
+    })
+  })
+
+  it('returns an editor for a C++ file', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + '/files/missing_include.cpp').then(editor => {
+        expect(lint.getValidEditor(editor)).toBeDefined();
+        // expect(lint.messages.length).toEqual(1)
+        // expect(messages[0].type).toEqual("error")
+      })
+    })
+  })
+
+  it('returns an editor for a C file', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + '/files/missing_include.cpp').then(editor => {
+        expect(lint.getValidEditor(editor)).toBeDefined();
+        // expect(lint.messages.length).toEqual(1)
+        // expect(messages[0].type).toEqual("error")
+      })
+    })
+  })
+
+  it('returns undefined for a javascript file', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + 'utility.js').then(editor => {
+        expect(lint.getValidEditor(editor)).not.toBeDefined();
+        // expect(lint.messages.length).toEqual(1)
+        // expect(messages[0].type).toEqual("error")
+      })
+    })
+  })
+})
